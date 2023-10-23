bb6652855a5d1942b5d402f2c63cd6382c77c8c1
3Ti <3Ti.Site@gmail.com>
2023-10-23 10:28:38 +0800

    ♨

--- binmap/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![allow(non_snake_case)]
-
-pub mod bin_map;
-
-pub type Result<T> = std::result::Result<T, wasm_bindgen::JsError>;
--- vbyte/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#![allow(non_snake_case)]
-
-pub mod vbyte;
-
-pub type Result<T> = std::result::Result<T, wasm_bindgen::JsError>;
